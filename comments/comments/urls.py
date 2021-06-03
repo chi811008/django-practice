c13a5438c47ac1303f633504e87b01fc6bba7e67
Seraphine_Tsui <seraphine.tsui@cloudmatrix.com.tw>
2021-06-03 20:11:50 +0800

    add: user serializer views urls

--- comments/comments/urls.py
+++ comments/comments/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from user import views as user_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', user_view.UsersView.as_view()),
 ]
